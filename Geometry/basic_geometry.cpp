378de536c2f5d661c912e1d5d087e44f7da1cf1d
Otito Amuga <urphasin@gmail.com>
2026-01-26 23:21:17 -0700

    flow.().exe

--- Geometry/basic_geometry.cpp
+++ Geometry/basic_geometry.cpp
@@ -9,6 +9,16 @@ double distance(Point A, Point B) {
     return sqrt((A.x - B.x)*(A.x - B.x) + (A.y - B.y)*(A.y - B.y));
 }
 
+Point rotate(Point A, double angle) {
+    double c = cos(angle);
+    double s = sin(angle);
+
+    return {
+        A.x * c - A.y * s,
+        A.x * s + A.y * c
+    };
+}
+
 int main() {
     printf("Distance of PointA(1, 1) to PointB(2, 2): %lf", distance((struct Point){1, 1}, (struct Point){2, 2})  );
 }
