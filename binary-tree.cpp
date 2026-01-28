709b76d7cdeabca7a2edd7842381084eec15d18f
Otito Amuga <urphasin@gmail.com>
2026-01-27 21:40:30 -0700

    binary.tree()

---
+++ binary-tree.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+
+int main() {
+    
+}
--- matrix.cpp
+++ matrix.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+long long permutate(int n, int r) {
+    long long result {1};
+
+    if (r > n) {
+        return n;
+    }
+
+    if (r < 0 || n < 0) {
+        return -1;
+    }
+
+    for (int i = 0; i < r; i++) {
+        result = result * (n - i); 
+    }
+
+    return result;
+}
+
+
+
 class Matrix {
     private:
     int rows, cols;
@@ -18,4 +38,5 @@ class Matrix {
 int main() {
     Matrix A(3, 3);
     cout << A.getCols() << "\n";
+    cout << permutate(10, 3) << "\n";
 }
